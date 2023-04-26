881906e1330cb932d5c3e6c33e7e235dba84b293
Jesus-0sorio <jaoz10.16@gmail.com>
2023-04-26 12:34:11 -0500

    actualizacion tabla visita coordinador

--- src/pages/subdirector/review/coordinator/Index.vue
+++ src/pages/subdirector/review/coordinator/Index.vue
@@ -22,10 +22,10 @@ onMounted(async () => {
 const search = ref('');
 
 const headers: Header[] = [
-	{ text: 'Nro.', value: 'id' },
+	// { text: 'Nro.', value: 'id' },
 	{ text: 'Fecha', value: 'date_visit' },
 	{ text: 'Municipio', value: 'municipalitie.name' },
-	// { text: 'Coordinador', value: 'created_by.name' },
+	//{ text: 'Coordinador', value: 'created_by.name' },
 	{ text: 'Escenario Deportivo', value: 'sports_scene' },
 	{ text: 'Estado', value: 'status' },
 	{ text: 'Acciones', value: 'actions' },
