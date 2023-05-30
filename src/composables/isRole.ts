521560f2c8dcd11c067c2e4c1f41b369668f6731
Jesus-0sorio <jaoz10.16@gmail.com>
2023-05-29 22:12:54 -0500

    fix side-menu

--- src/composables/isRole.ts
+++ src/composables/isRole.ts
@@ -3,14 +3,15 @@ import { onboardingStore } from "@/stores/onboardingStore"
 
 export const isRole = (role: roles, user: any = null): boolean => {
     let findRole = null;
-
+    
     if (user) {
         findRole = user.roles.some((_role: any) => _role.slug == role)
-    } else if (role.length > 1) {
-        const store = onboardingStore()
-        findRole = store.userRoles?.some((userRole) => role.includes(userRole.slug))
-    }
-    else {
+    } else if (Array.isArray(role)) {
+      const store = onboardingStore();
+      findRole = store.userRoles?.some((userRole) =>
+        role.includes(userRole.slug)
+      );
+    } else {
         const store = onboardingStore()
         findRole = store.userRoles?.some((userRole) => userRole.slug == role)
     }
