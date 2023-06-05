807fe102bfd865f6742fd85d392c0a9a91d253ad
DiegoAlejandroGarzon <tingonay10@hotmail.com>
2023-06-05 14:12:48 -0500

    SSIS-240 cargar municipio segun user auth

--- src/composables/getSelect.ts
+++ src/composables/getSelect.ts
@@ -1,8 +1,8 @@
 import qs from 'qs'
 type Select = 'municipalities' | 'groups' | 'validity_periods' | 'zones' | 'statuses' | 'disciplines' | 'roles' | 'departments' | 'bank_account_types' | 'identification_types' | 'banks' | 'objects' | 'months' | 'asistentList' | 'metodologoList';
 
-export const getSelect = async (selects: Select[]) => {
-    const parse = qs.stringify({ selects: selects })
+export const getSelect = async (selects: Select[], auth = false) => {
+    const parse = qs.stringify({ selects: selects, auth })
     const response = await api.get(`/api/v1/selects?${parse}`)
 
     return response.data.at(0) as Array<{ label: string, value: string | number }>
