1874297e1260cddd491dcf366cbd93792e2e81e5
4RCH1T3CT <adscalicolombia@gmail.com>
2023-06-15 01:01:17 -0500

    cambio boton descarga zip

--- src/pages/coordinators/Index.vue
+++ src/pages/coordinators/Index.vue
@@ -23,21 +23,21 @@ const headers: Header[] = [
 const search = ref('');
 const data = computed(() => searchData(items.value, search.value));
 
-const downloadAllMeto = async()=>{
-	await coordinatorVisitServices.downloadFilesMeto().then((response)=>{
-				if (response) {
-					if (response.status >= 200 && response.status <= 300) {
-						console.log(response);
-						//setLoading(true);
-						/*router.push({ name: 'coordinator.index' }).finally(() => {
-							setLoading(false);
-						});*/
-					}
-				} else {
-					console.log("error");
-				}
-	}).catch((error)=>{console.log(error)})
-}
+// const downloadAllMeto = async()=>{
+// 	await coordinatorVisitServices.downloadFilesMeto().then((response)=>{
+// 				if (response) {
+// 					if (response.status >= 200 && response.status <= 300) {
+// 						console.log(response);
+// 						//setLoading(true);
+// 						/*router.push({ name: 'coordinator.index' }).finally(() => {
+// 							setLoading(false);
+// 						});*/
+// 					}
+// 				} else {
+// 					console.log("error");
+// 				}
+// 	}).catch((error)=>{console.log(error)})
+// }
 const downloadAllCoor = async()=>{
 	await coordinatorVisitServices.downloadFilesCoor().then((response)=>{
 				if (response) {
@@ -53,21 +53,21 @@ const downloadAllCoor = async()=>{
 				}
 	}).catch((error)=>{console.log(error)})
 }
-const downloadAll = async()=>{
-	await coordinatorVisitServices.downloadFiles().then((response)=>{
-				if (response) {
-					if (response.status >= 200 && response.status <= 300) {
-						console.log(response);
-						//setLoading(true);
-						/*router.push({ name: 'coordinator.index' }).finally(() => {
-							setLoading(false);
-						});*/
-					}
-				} else {
-					console.log("error");
-				}
-	}).catch((error)=>{console.log(error)})
-}
+// const downloadAll = async()=>{
+// 	await coordinatorVisitServices.downloadFiles().then((response)=>{
+// 				if (response) {
+// 					if (response.status >= 200 && response.status <= 300) {
+// 						console.log(response);
+// 						//setLoading(true);
+// 						/*router.push({ name: 'coordinator.index' }).finally(() => {
+// 							setLoading(false);
+// 						});*/
+// 					}
+// 				} else {
+// 					console.log("error");
+// 				}
+// 	}).catch((error)=>{console.log(error)})
+// }
 </script>
 
 <template>
@@ -75,24 +75,24 @@ const downloadAll = async()=>{
 		<h2 class="mr-auto text-lg font-medium">
 			Listado Visitas de coordinadores
 		</h2>
-		<Button
+		<!-- <Button
 			variant="primary"
 			class="btn btn-primary"
 			@click="downloadAllMeto">
 			Descargar Meto
-		</Button>
+		</Button> -->
 		<Button
 			variant="primary"
 			class="btn btn-primary"
 			@click="downloadAllCoor">
-			Descargar Coor
+			Descargar Zips
 		</Button>
-		<Button
+		<!-- <Button
 			variant="primary"
 			class="btn btn-primary"
 			@click="downloadAll">
 			Descargar Zips
-		</Button>
+		</Button> -->
 	</div>
 	<!-- BEGIN: Page Layout -->
 	<div class="p-5 mt-5 intro-y box">
