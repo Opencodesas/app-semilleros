5aec9a18674d8b87adaee7c050a3e92fd60cdcc3
DiegoAlejandroGarzon <tingonay10@hotmail.com>
2023-06-25 10:00:17 -0500

    SSIS413 cargar monitores

--- src/composables/getSelect.ts
+++ src/composables/getSelect.ts
@@ -1,5 +1,5 @@
 import qs from 'qs'
-type Select = 'municipalities' | 'groups' | 'validity_periods' | 'zones' | 'statuses' | 'disciplines' | 'roles' | 'departments' | 'bank_account_types' | 'identification_types' | 'banks' | 'objects' | 'months' | 'asistentList' | 'metodologoList' | 'managerList';
+type Select = 'municipalities' | 'groups' | 'validity_periods' | 'zones' | 'statuses' | 'disciplines' | 'roles' | 'departments' | 'bank_account_types' | 'identification_types' | 'banks' | 'objects' | 'months' | 'asistentList' | 'metodologoList' | 'managerList' | 'monitors';
 
 export const getSelect = async (selects: Select[], auth = false) => {
     const parse = qs.stringify({ selects: selects, auth })
