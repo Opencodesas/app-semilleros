5793fe5e15dcdf41aeac78d24e50085620336f05
DiegoAlejandroGarzon <tingonay10@hotmail.com>
2023-07-08 22:36:59 -0500

    SSS20 fix

--- src/pages/forms/first_form.vue
+++ src/pages/forms/first_form.vue
@@ -515,7 +515,7 @@ const form_rules_acudiente = computed(() => ({
   apellidosAcudiente: { required },
   nDocuAcudiente:     { required },
   parentesco:         { required },
-  // email:              { required },
+  email:              {},
   // checkEmail:      { required },
   nCelularAcudiente:  { required },
   // checkNCel:       { required },
